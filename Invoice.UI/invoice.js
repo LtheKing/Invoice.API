d7497c26f96d567ebbeb49c42a7d6044b90e6c64
Leonaldi Aldi <leonalding77@gmail.com>
2021-03-09 22:45:40 +0700

    add calculate function, fixing dynamic content

--- Invoice.UI/invoice.js
+++ Invoice.UI/invoice.js
@@ -41,12 +41,56 @@ function btnResetSenderOnClick() {
 
 //add new line
 function btnNewLineOnClick() {
-    var i = 0;
-    var original = document.getElementById('bottomContent' + i);
-    var clone = original.cloneNode(true); // "deep" clone
-    clone.id = "bottomContent" + ++i; // there can only be one element with an ID
-    //clone.onclick = btnNewLineOnClick; // event handlers are not cloned
-    original.parentNode.appendChild(clone);
+    // debugger;
+    // var i = 0;
+    // var original = document.getElementById('bottomContent' + i);
+    // var clone = original.cloneNode(true); // "deep" clone
+    // clone.id = "bottomContent" + ++i; // there can only be one element with an ID
+    // //clone.onclick = btnNewLineOnClick; // event handlers are not cloned
+    // original.parentNode.appendChild(clone);
+
+    var max_fields = 25;
+    var wrapper = $('.multiContent');
+    var add_button = $('.btnNewLine');
+    var x = 0;
+    if (x < max_fields) {
+        x++;
+        $(wrapper).append(`
+        <div class="row induk">
+        <div class="col-4">
+              <label for="ContentNameLabel">Name</label>
+              <textarea class="form-control" id="txt_contentName" name="ContentName" rows="4" cols="50"></textarea>
+            </div>
+            <div class="col">
+              <label for="Quantity">Quantity</label>
+              <input type="number" name="Quantity" id="txt_quantity" class="form-control c_qty" value="0">
+            </div>
+            <div class="col">
+              <label for="ContentName">Rate</label>
+              <input type="number" name="Rate" id="txt_rate" class="form-control c_rate" value="1000">
+              <select class="form-control mt-2" id="select_rate">
+                <option>hour</option>
+                <option>day</option>
+              </select>
+            </div>
+            <div class="col">
+              <label for="Amount">Amount</label>
+              <input type="number" name="Amount" id="txt_amount" class="form-control c_amount">
+            </div>
+            <hr style="width:100%;text-align:left;margin-left:0"></div> `);
+
+        $('.c_qty').change(function (e) {
+            var rate = $(e.target).closest('.induk').find('.c_rate').val();
+            var field_amount = $(e.target).closest('.induk').find('.c_amount')
+            calculateAmountMulti(e.target.value, rate, field_amount)
+        });
+
+        $('.c_rate').change(function(e) {
+            var qty = $(e.target).closest('.induk').find('.c_qty').val();
+            var field_amount = $(e.target).closest('.induk').find('.c_amount')
+            calculateAmountMulti(qty, e.target.value, field_amount)
+        })
+    }
 }
 
 function onSelectCurrency() {
@@ -170,5 +214,40 @@ function setNomorPO() {
 
 //content editor on change
 function onContentEditorChange() {
+
+}
+
+//calculate initial amount
+function calculateAmount() {
     
+}
+
+//calculate amount multi
+function calculateAmountMulti(qty, rate, field_amount) {
+    var tot = qty * rate;
+    field_amount.val(tot);
+}
+
+//calculate total
+function calculateTotal() {
+    var disc = document.getElementById('txt_discount').value;
+    var subTot = document.getElementById('txt_subtotal').value;
+    var tot = subTot - disc;
+    document.getElementById('txt_total').value = tot;
+}
+
+//calculate diskon
+function calculateDiskon() {
+    var discPersen = document.getElementById('txt_discountPersentation').value / 100;
+    var disc = document.getElementById('txt_discount').value;
+    var subTot = document.getElementById('txt_subtotal').value;
+
+    var res = discPersen * subTot;
+    document.getElementById('txt_discount').value = res;
+}
+
+//calculate blok bawah
+function calculateBlokBawah() {
+    calculateDiskon();
+    calculateTotal();
 }
